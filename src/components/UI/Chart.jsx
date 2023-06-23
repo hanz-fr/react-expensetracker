9819e59930c4955400fd95546f920dd7531de096
Han <59479453+recta-rrae@users.noreply.github.com>
2023-06-23 22:35:51 +0700

    (components) : experimenting with alternate way to display ChartBar

--- src/components/UI/Chart.jsx
+++ src/components/UI/Chart.jsx
@@ -4,11 +4,23 @@ import Container from "./Container";
 import ChartBar from "./ChartBar";
 
 const ExpenseChart = (props) => {
-
-  
   return (
     <div>
       <Container className="bg-[#373737] border-[6px] border-zinc-800 flex justify-between md:justify-evenly md:gap-0 rounded-xl py-5 px-[10px]">
+        /* 
+        Improved version of ChartBar.
+        ChartBar is displayed dynamically, by using an array and then mapping it.
+        It has an id using label (month) as its value, which is unique.
+        Value holds a total of expenses each month
+        */
+        {props.dataPoints.map((dataPoint) => (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={null}
+            label={dataPoint.label}
+          />
+        ))}
         <ChartBar month={"Jan"} />
         <ChartBar month={"Feb"} />
         <ChartBar month={"Mar"} />
--- src/components/UI/ChartBar.jsx
+++ src/components/UI/ChartBar.jsx
@@ -10,12 +10,20 @@ const ChartBar = (props) => {
   z = percentage each month : (1/x * y) * 100
   */
 
+
+  /* chart bar formula from udemy */
+  let barHeight = '0%';
+
+  if (props.max > 0) {
+    barHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+  }
+
   return (
   <div className="flex flex-col gap-3">
       <div className="chart-bar mx-auto">
-        <div className="bg-[#00CCBF] overflow-hidden transition-all duration-500 rounded-t-[4px]" role="progressbar" style={{ height: "0%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+        <div className="bg-[#00CCBF] overflow-hidden transition-all duration-500 rounded-t-[4px]" role="progressbar" style={{ height: barHeight }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
       </div>
-      <div className="text-white font-light text-[10px] md:text-sm">{props.month}</div>
+      <div className="text-white font-light text-[10px] md:text-sm">{props.label}</div>
     </div>
   );
 };
